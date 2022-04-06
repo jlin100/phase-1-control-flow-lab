24452eba04e437c9f0f68d8eaaff7cadb40029ec
Jeff Lin <thejefflin@gmail.com>
2022-04-06 16:30:35 -0400

    commit tree a8bda6d96fdfbf58f509198f01d48eac1d27c514

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(a){
   // Write your code here!
+  if (a <= 400){
+    return "This one is on me!"
+  } else if (a > 400 && a < 2000) {
+    return "That will be twenty bucks."
+  } else if (a >=2000 && a <2500) {
+    return "I will gladly take your thirty bucks."
+  } else if (a >= 2500) {
+    return "No can do."
+  } 
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(c){
   // Write your code here!
+return (c == "NYC" ? "Ok, sounds good." : "No go.");
+} 
+
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+switch (tip) {
+  case "generous":
+    return "Thank you so much.";
+  case "not as generous":
+    return "Thank you.";
+  default:
+    case "thanks for everything":
+      return "Bye."
+}
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
